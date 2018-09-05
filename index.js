a891a1bc79d6a2e824bac97e3a5e504411d5cdbf
Ana Harris <vana1990@gmail.com>
2018-09-05 18:46:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -28,6 +28,6 @@ function prependKitten(name) {
 }
 
 function removeLastKitten() {
-  var newArray = kittens.slice(kittens.length - 1);
+  var newArray = kittens.slice(0, -1);
   return newArray;
 }
